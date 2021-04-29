ef8ed629a8395fdeac37b3168b3abd214c7d4aee
Ziyi <wangziyi71600@gmail.com>
2021-04-30 01:29:53 +0800

    time magazine page finished

--- final/scripts.js
+++ final/scripts.js
@@ -2,3 +2,35 @@ var loadPhoto = function(event) {
     var image = document.getElementById('photoOutput');
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+function ttext1Change() {
+    var x = document.getElementById("mytext1");
+    var defaultVal = x.defaultValue;
+    var currentVal = x.value;
+    
+    
+      document.getElementById("ttext1").innerHTML = currentVal
+    
+  }
+
+  function ttext3Change() {
+    var x = document.getElementById("mytext3");
+    var defaultVal = x.defaultValue;
+    var currentVal = x.value;
+    
+    
+      document.getElementById("text3").innerHTML = currentVal
+    
+  }
+function changeTextColor(){
+  var e = document.getElementById("colorChoice");
+    var result = e.options[e.selectedIndex].value;
+    document.getElementById("ttext1").style.color = result
+    document.getElementById("ttext3").style.color= result;
+}
+
+function changeFilter(){
+    var e = document.getElementById("filterChoice");
+      var result = e.options[e.selectedIndex].value;
+      document.getElementById("photoOutput").style.filter = result
+  }
